ec6be5aa675b942a6282cbf3205585067033c00a
Zachary Bush <Zachary.bush@meraki.net>
2014-04-08 17:02:44 -0700

    Move templates to ejs instead of jade

--- server/app.js
+++ server/app.js
@@ -9,12 +9,15 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var ejs = require('ejs-locals');
+
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('ejs', ejs);
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
