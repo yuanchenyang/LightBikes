95d3238f16571ee07b81530e00181279571d9037
Zachary Bush <Zachary.bush@meraki.net>
2014-04-08 17:23:48 -0700

    Start adding submit page. Switch app to run with nodemon

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var _ = require('underscore');
 
 var ejs = require('ejs-locals');
 
@@ -31,8 +31,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+_.each(routes, function(func, name) {
+  var route = '/' + name;
+  if (name === 'index') {
+    route = '/';
+  }
+  app.get(route, func);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,15 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Light Bikes'
+  });
+};
+
+exports.submit = function(req, res) {
+  res.render('submit', {
+    title: 'Light Bikes - Submit Entry'
+  });
 };
