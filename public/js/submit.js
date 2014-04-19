774731e812a3ec66d0dddb536172429358496b5b
Zachary Bush <Zachary.bush@meraki.net>
2014-04-18 23:08:35 -0700

    Get custom bots working
    Switch to lodash

--- public/js/submit.js
+++ public/js/submit.js
@@ -9,6 +9,16 @@ $(document).ready(function() {
   });
   var b1s = $("#bot1_select");
   var b2s = $("#bot2_select");
+
+  dummy_code = [
+    "Bot.register('DummyBot', function(board_state, player_state, move) {",
+    "  // Bot code, then call move!",
+    "})"
+  ].join("\n");
+
+  win1.setValue(dummy_code);
+  win2.setValue(dummy_code);
+
   b1s.change(function(event) {
     var el = $(win1.getWrapperElement());
     var category = event.target.value;
@@ -32,10 +42,62 @@ $(document).ready(function() {
 
   var subm = $('#start_game_btn');
   subm.click(function(event) {
-    botNames = [b1s.val(), b2s.val()];
-    var game = new Game(botNames, false);
-    game.run(function(result) {
-      console.log(result);
-    });
+    wait = 0;
+    var a,b;
+    if (b1s.val() === 'custom') {
+      wait += 1;
+      injectJS(win1.getValue(), function(v) {
+        a = v;
+        tryRun();
+      });
+    } else {
+      a = b1s.val();
+    }
+    if (b2s.val() === 'custom') {
+      wait += 1;
+      injectJS(win2.getValue(), function(v) {
+        b = v;
+        tryRun();
+      });
+    } else {
+      b = b2s.val();
+    }
+
+    function injectJS(str, cb) {
+      var gcb = _.uniqueId("__JS_CB");
+
+      window[gcb] = function(n) {
+        cb(n);
+        delete window[gcb];
+      };
+
+      str = gcb + "(" + str + ");";
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      try {
+        s.appendChild(document.createTextNode(str));
+        document.body.appendChild(s);
+      } catch (e) {
+        s.text = str;
+        document.body.appendChild(s);
+      }
+    }
+
+    if (wait === 0) run();
+
+    function tryRun() {
+      if (a && b) {
+        run();
+      }
+    }
+
+    function run() {
+      botNames = [a, b];
+      var game = new Game(botNames, false);
+      game.run(function(result) {
+        console.log(result);
+      });
+    }
   });
+
 });
